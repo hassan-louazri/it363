d1acbb401a6f99ce4a44a46ebde4e01a2c2c28e3
Louazri Hassan <hassan.louazri@bordeaux-inp.fr>
2022-11-08 09:45:51 +0100

    conflict resolved

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:project_basic_quiz/home.dart';
 
-import 'package:project_basic_quiz/main.dart';
+// import 'package:project_basic_quiz/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
